bbed86924be6423fd83bdef6b43e6d5510b7f3a0
CRUKREM <56958860+CRUKREM@users.noreply.github.com>
2019-11-13 09:59:33 +0000

    Create player.js

---
+++ player.js
@@ -0,0 +1,18 @@
+{
+  "backURL": "https://192.168.1.123:8125/api/AR.js-2.0.0/three.js/examples/multi-markers/examples/player.html#%7B%22trackingBackend%22%3A%22artoolkit%22%7D",
+  "trackingBackend": "artoolkit",
+  "detectionMode": "mono_and_matrix",
+  "markersControlsParameters": [
+    {
+      "type": "barcode",
+      "barcodeValue": "0"
+    },
+    {
+      "type": "barcode",
+      "barcodeValue": "1"
+    },
+    {
+      "type": "barcode",
+      "barcodeValue": "2"
+    },
+]}
